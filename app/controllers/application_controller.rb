788f80ec050fa92149f3630a243bdcefe20985c1
Will Carter <will.carter@fergusllc.com>
2020-03-11 13:38:13 -0600

    add sinatra site

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,18 @@
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, 'secure_password'
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/error' do
+    erb :error
+  end
+
+end
