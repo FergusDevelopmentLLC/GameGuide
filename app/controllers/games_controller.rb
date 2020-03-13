1c46c02c57920d4bc6ca847eda07ee54d7f5f4dd
Will Carter <will.carter@fergusllc.com>
2020-03-13 16:53:09 -0600

    game show stub

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -25,6 +25,10 @@ class GamesController < ApplicationController
         end
     end
 
+    get '/games/:id' do
+        @game = Game.find(params[:id])
+        erb :'/games/show'
+    end
     
 end
 
