6617fa493ee53168af569c5dea83387ae0308536
Will Carter <will.carter@fergusllc.com>
2020-03-16 09:57:44 -0600

    refactor game controller to protect routes per login conditions

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,18 +2,40 @@ require 'rack-flash'
 class GamesController < ApplicationController
     use Rack::Flash
     
+    get "/games" do
+        @games = Game.all
+        erb :'games/index'
+    end
+
     get '/games/new' do
         if !logged_in?
-            flash[:message] = "Please log in to create or edit a game."
+            flash[:message] = "Please log in to create a game."
             redirect "/games"
         else
             erb :'games/new'
         end
     end
 
+    get '/games/:id' do
+        @game = Game.find(params[:id])
+        erb :'/games/show'
+    end
+
+    get '/games/:id/edit' do
+        @game = Game.find(params[:id])
+
+        if logged_in? && can_edit_game?(@game)
+            erb :'/games/edit'
+        else
+            flash[:message] = "You must be logged in and owner of a game to edit it."
+            redirect "/games/#{@game.id}"
+        end
+    end
+
     post '/games' do
         
         if logged_in?
+
             @game = Game.new(params[:game])
             @game.user = current_user
 
@@ -39,76 +61,68 @@ class GamesController < ApplicationController
             end
                
         else
-            flash[:message] = "Please log in to create or edit a game."
+            flash[:message] = "You must be logged in to create a game."
             redirect "/games"
         end
     end
 
-    get '/games/:id' do
-        @game = Game.find(params[:id])
-        erb :'/games/show'
-    end
 
     patch "/games/:id" do
         
         @game = Game.find(params[:id])
 
-        if @game.update(params[:game]) 
-            
-            @game.tags.clear
+        if logged_in? && can_edit_game?(@game)
 
-            if params[:tag_ids]
-                params[:tag_ids].each {|tag_id|
-                    tag = Tag.find(tag_id)
+            if @game.update(params[:game]) 
+                
+                @game.tags.clear
+
+                if params[:tag_ids]
+                    params[:tag_ids].each {|tag_id|
+                        tag = Tag.find(tag_id)
+                        if !@game.tags.include?(tag)
+                            #cant user @game << tag here because need user_id on the GameTagUser record
+                            GameTagUser.create(:game => @game, :tag => tag, :user => current_user)   
+                        end
+                    }
+                end
+
+                if !params[:new_tag].empty?
+                    tag = Tag.find_or_create_by(:name => params[:new_tag].downcase)
+                    
                     if !@game.tags.include?(tag)
                         #cant user @game << tag here because need user_id on the GameTagUser record
-                        GameTagUser.create(:game => @game, :tag => tag, :user => current_user)   
+                        GameTagUser.create(:game => @game, :tag => tag, :user => current_user)
                     end
-                }
-            end
-
-            if !params[:new_tag].empty?
-                tag = Tag.find_or_create_by(:name => params[:new_tag].downcase)
-                
-                if !@game.tags.include?(tag)
-                    #cant user @game << tag here because need user_id on the GameTagUser record
-                    GameTagUser.create(:game => @game, :tag => tag, :user => current_user)
                 end
-            end
 
-            if @game.save
-                flash[:message] = "Game updated successfully."
-            end
+                if @game.save
+                    flash[:message] = "Game updated successfully."
+                end
 
+            end
+        else
+            flash[:message] = "You must be logged in and owner of a game to edit it."
         end
 
         redirect "/games/#{@game.id}"
     end
 
-    get "/games" do
-        @games = Game.all
-        erb :'games/index'
-    end
-
-    get '/games/:id/edit' do
+    delete '/games/:id' do
         @game = Game.find(params[:id])
 
         if logged_in? && can_edit_game?(@game)
-            erb :'/games/edit'
+            @game.destroy
+            #delete any orphaned tags as a result
+            Tag.all.find_all {|tag| tag.games.empty?}.each {|tag| tag.destroy}
+            flash[:message] = "Game deleted."
+            redirect "/games"
         else
-            flash[:message] = "Please log in to create or edit a game."
+            flash[:message] = "You must be logged in and owner of a game to edit it."
             redirect "/games/#{@game.id}"
         end
+        
     end
 
-    delete '/games/:id' do
-        @game = Game.find(params[:id])
-        @game.destroy
-        #delete any orphaned tags as a result
-        Tag.all.find_all {|tag| tag.games.empty?}.each {|tag| tag.destroy}
-        flash[:message] = "Game deleted."
-        redirect "/games"
-    end
-    
 end
 
