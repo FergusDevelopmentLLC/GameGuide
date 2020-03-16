821166c3202f7439cfdf6fd48b4203845b4045ba
Will Carter <will.carter@fergusllc.com>
2020-03-16 09:24:20 -0600

    protect comment routes to make sure user is logged in

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,37 +3,58 @@ class CommentsController < ApplicationController
     use Rack::Flash
 
     get "/comments/:id/edit" do
-        @comment = Comment.find(params[:id])
-        erb :'comments/edit'
+        if logged_in?
+            @comment = Comment.find(params[:id])
+            erb :'comments/edit'
+        else
+            flash[:message] = "Please log in to comment on a game."
+            redirect "/games"
+        end
     end
 
     post "/comments" do
-        @game = Game.find(params[:game_id])
+        if logged_in?
 
-        if !params[:show_new_comment].empty?
-            new_comment = Comment.new(:content => params[:show_new_comment])
-            new_comment.user = current_user
-            @game.comments << new_comment
-            flash[:message] = "Thank you for adding a comment."
+            @game = Game.find(params[:game_id])
+
+            if !params[:show_new_comment].empty?
+                new_comment = Comment.new(:content => params[:show_new_comment])
+                new_comment.user = current_user
+                @game.comments << new_comment
+                flash[:message] = "Thank you for adding a comment."
+            else
+                flash[:add_comment_message] = "Please enter a comment."
+            end
+            
+            redirect "/games/#{@game.id}"
         else
-            flash[:add_comment_message] = "Please enter a comment."
+            flash[:message] = "Please log in to comment on a game."
+            redirect "/games"
         end
-        
-        redirect "/games/#{@game.id}"
     end
     
     patch "/comments/:id" do
-        @comment = Comment.find(params[:id])
-        @comment.update(params[:comment])
-        flash[:message] = "Comment updated."
-        redirect "/games/#{@comment.game.id}"
+        if logged_in?
+            @comment = Comment.find(params[:id])
+            @comment.update(params[:comment])
+            flash[:message] = "Comment updated."
+            redirect "/games/#{@comment.game.id}"
+        else
+            flash[:message] = "Please log in to update a game comment."
+            redirect "/games"
+        end
     end
 
     delete "/comments/:id" do
-        @comment = Comment.find(params[:id])
-        @comment.destroy
-        flash[:message] = "Comment deleted."
-        redirect "/games/#{@comment.game.id}"
+        if logged_in?
+            @comment = Comment.find(params[:id])
+            @comment.destroy
+            flash[:message] = "Comment deleted."
+            redirect "/games/#{@comment.game.id}"
+        else
+            flash[:message] = "Please log in to update a game comment."
+            redirect "/games"
+        end
     end
 end
 
